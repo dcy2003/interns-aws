1adecc71f7833189157f44e6ed8562461f97b174
Derek Yeager <dcy2003@gmail.com>
2016-07-05 07:51:38 -0400

    cleanup

--- httpRequestHandler.js
+++ httpRequestHandler.js
@@ -1,55 +1,56 @@
+// Require Third Party Libraries
 var request = require('request');
 var AWS = require('aws-sdk');
 var DynamoDB = new AWS.DynamoDB.DocumentClient();
 var _ = require('lodash');
-
-var isEmpty = function(str) {
-	return str === '';
-}
+var uuid = require('node-uuid');
 
 exports.issueRequestHandler = function(event, context, callback) {
-
-	console.log('remaining time =', context.getRemainingTimeInMillis());
-    console.log('functionName =', context.functionName);
-    console.log('AWSrequestID =', context.awsRequestId);
-    console.log('logGroupName =', context.logGroupName);
-    console.log('logStreamName =', context.logStreamName);
-    console.log('clientContext =', context.clientContext);
 
     // stoppoint arrivals for StopPointId = 940GZZLUASL
     var endpoint = 'https://api.tfl.gov.uk/StopPoint/940GZZLUASL/Arrivals?app_id=51696fcf&app_key=d1105f80ed4ceaf95b9f230d88e2770f';
 
     request(endpoint, function(error, response, body) {
+
 		if(!error && response.statusCode == 200) {
+
+			var responseTime = new Date();
 			var json = JSON.parse(body);
 			var numEntries = json.length;
 			console.log("Received " + numEntries + " items.");
+
 			// iterate items
 			for(var i in json) {
-				json[i].stopPointId = '940GZZLUASL'; // decorate item w/ StopPointId
-				var item = _.omitBy(json[i], isEmpty); // omit empty strings since Dynamo does not like them
-				console.log(item);
+
+				json[i].uuid = uuid.v4(); 						// decorate item with UUID
+				json[i].stopPointId = '940GZZLUASL'; 			// decorate item with StopPointId
+				json[i].responseTime = responseTime;			// decorate item with time of response
+				json[i].awsRequestId = context.awsRequestId;	// decorate item with Lambda request ID
+				
+				// Dynnamo does not like empty strings, omit them
+				var item = _.omitBy(json[i], function(str) { return str === ''; });
+
+				//console.log(item);
+
+				// Build DynamoDB entry
 				var params = {
 				    TableName: "StopPointArrivals",
 				    Item: item
 				};
+
+				// Insert into DynamoDB
 				DynamoDB.put(params, function (err, data) {
-				    if (err) {
-				        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-				    } else {
-				        console.log("Added item:", JSON.stringify(data, null, 2));
-				    }
+				    if (err) console.error("Unable to add item.");
+				    else console.log("Added item");
 				});
 			}
-			callback(null, numEntries); // return response
+
+			// return response
+			callback(null, numEntries);
 		}
 		else {
 			console.error("Received an error or a non 200 response.");
 			callback("Received an error or a non 200 response.");
 		}
 	});
-
-	// TODO externalize requests and require them
-	// TODO How to capture metadata such as request date, time, type
-	// TODO Documentation
 };
